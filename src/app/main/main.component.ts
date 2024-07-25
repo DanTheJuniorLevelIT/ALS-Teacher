20496735bf30f0cf5f12c7c8dfae148b559e49a8
Dan Malayo <danielmalayo2@gmail.com>
2024-07-25 13:07:44 +0800

    Router Outlet updated

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
