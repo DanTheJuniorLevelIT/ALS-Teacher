1003104080d74bd75812562e132be3c18e9eef5b
Dan Malayo <danielmalayo2@gmail.com>
2024-07-25 15:46:56 +0800

    Update sidenav add Title

--- src/app/Modules/Home/home.routes.ts
+++ src/app/Modules/Home/home.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomemainComponent } from './homemain/homemain.component';
+import { HomeComponent } from './home/home.component';
 
-export const homeRoute: Routes = [];
+export const homeRoute: Routes = [
+    {path: 'main', component: HomemainComponent, 
+        children: [
+            {path: 'home', component: HomeComponent},
+            {path: '', redirectTo: 'home', pathMatch: 'full'}
+        ]
+    },
+    {path: '', redirectTo: 'main', pathMatch: 'full'}
+];
--- src/app/Modules/Home/homemain/homemain.component.ts
+++ src/app/Modules/Home/homemain/homemain.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homemain',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './homemain.component.html',
   styleUrl: './homemain.component.css'
 })
